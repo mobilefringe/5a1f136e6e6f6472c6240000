7108f149238bd21c85ec180085696c4646b29aca
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 17:30:10 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -49,6 +49,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
       return {
         dataLoaded: false,
         show_mobile_menu : false,
+        is_searching : false,
         menu_items: [
             // {
             //     name: "Home",
