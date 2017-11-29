ffcf049f2cf06c3ce62e50da3a2205583d9bcd92
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 15:43:31 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -120,10 +120,6 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             console.log(this.$store.state.results.banners);
             return this.$store.state.results.banners;
         },
-        getMainBanner () {
-            console.log( _.filter(this.getBanners, { 'position': 1})[0]);
-            return  _.filter(this.getBanners, { 'position': 1})[0];
-        },
         copyright_year() {
             return moment().year();
         },
