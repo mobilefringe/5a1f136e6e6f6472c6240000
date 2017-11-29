d4caec4c34c73d5ab6670c6b788dbcdedf25c75a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 15:43:39 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -127,8 +127,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             return this.$store.getters.getProperty;
         },
         hours(){
-            var hours = 
-            _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
+            var hours = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
             console.log(hours);
             return hours;
         },
