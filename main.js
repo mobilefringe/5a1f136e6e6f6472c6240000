3e57fa1bc956cbe327d543924cea155dbf4c2523
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 15:36:51 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -54,55 +54,36 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 name: "Home",
                 id: "home_link",
                 href: "/",
-                parent_id: "home",
-                parent_class_list: "menu_item",
-                hover_id: "home_underline",
-                hover_class_list:"menu_underline"
+                parent_id: "home"
             },
             {
                 name: "Store Directory",
                 id: "stores_link",
-                href: "/stores",
-                parent_id: "stores",
-                parent_class_list: "menu_item",
-                hover_id: "stores_underline",
-                hover_class_list:"menu_underline"
+                href: "/stores"
             },
             {
                 name: "Sales & Promos",
                 id: "sales_link",
                 href: "/promotions",
-                parent_id: "sales",
-                parent_class_list: "menu_item",
-                hover_id: "sales_underline",
-                hover_class_list:"menu_underline"
+                parent_id: "sales"
             },
             {
                 name: "Events",
                 id: "events_link",
                 href: "/",
-                parent_id: "events",
-                parent_class_list: "menu_item",
-                hover_id: "events_underline",
-                hover_class_list:"menu_underline"
+                parent_id: "events"
             },
             {
                 name: "Careers",
                 id: "jobs_link",
-                href: "/",
-                parent_id: "jobs",
-                parent_class_list: "menu_item",
-                hover_id: "jobs_underline",
-                hover_class_list:"menu_underline"
+                href: "/jobs",
+                parent_id: "jobs"
             },
             {
                 name: "Store Hours",
                 id: "hours_link",
-                href: "/",
-                parent_id: "hours",
-                parent_class_list: "menu_item",
-                hover_id: "hours_underline",
-                hover_class_list:"menu_underline"
+                href: "/hours",
+                parent_id: "hours"
             }
         ]
       }
