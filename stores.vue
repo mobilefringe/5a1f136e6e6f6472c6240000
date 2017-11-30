e4154ba5d4ba163906c6ee623c7c0c965eadbaa6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 13:32:27 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -89,8 +89,7 @@
                             <div class="store_list_content cats_row" data-cat="{{cat_list}}">
                                 <p class="store_name"><a href="/stores/{{slug}}">{{name}}</a></p>
                             </div>
-                        </div>
-                        <div class="store_list" id="store_list_container"></div>       
+                        </div>   
                         </div>
                     </div>
                     <!--<div class="col-md-6 store_col_2">-->
