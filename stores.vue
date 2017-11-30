b2c2eedb347bc87397e60211dc0e7b9e417fc14e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 14:26:07 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -2,7 +2,7 @@
     <div class="content_container main_container position_relative padding_top_20">
         <div class="map_container">
             <div id="map" class="hidden_phone">
-                <<img src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1511892740000/Canyon Crest - Map-01.png" class="" alt="">
+                <img src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1511892740000/Canyon Crest - Map-01.png" class="" alt="">
             </div>
             <!--<svg-map :svgMapUrl="getSVGurl"></svg-map>-->
         </div>
