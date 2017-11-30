10ad53ce30234234d819e41d7aaa7a8a02ccb8ea
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 11:38:19 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
         <slick ref="slick" :options="slickOptions">
-            <div v-for="banner in banners" >
+            <div v-for="banner in banners" v-if="banners">
                 <div :style="'background-image:url('+banner.image_url+')'" class="banner_image_div">
                     <router-link :to="banner.url" class="banner_image_li"></router-link>
                 </div>
