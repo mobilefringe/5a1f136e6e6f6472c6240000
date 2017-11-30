eb7f89a6917c4cb5a0f7756c95cf641e0f0149a3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 13:38:22 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -83,7 +83,7 @@
                 <h5 class="category_header" style="display:none" id="cat_name_header">All</h5>
                 <div class="row">
                     <div class="col-md-6 store_col_1">
-                    <div v-for="(store,key) in storesByAlphaIndex">
+                    <div v-for="(stores,key) in storesByAlphaIndex">
                         <span class="store_initial open_stores" data-initial="number">{{key}}</span>
                         <div id="numbered_store_list_container" class="">
                             <div class="store_list_content cats_row" :data-cat="store.cat_list">
