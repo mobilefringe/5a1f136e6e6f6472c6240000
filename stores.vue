34dcc32d627dcea5c67c079fd856cba3110991fc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 13:25:44 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -157,6 +157,15 @@
         },
         storesByCategoryName() {
           return this.$store.getters.storesByCategoryName;
+        },
+        getSVGurl () {
+            return "https://www.mallmaverick.com" + this.property.svgmap_url;
+        },
+        allStores() {
+            return this.$store.getters.processedStores;
+        },
+        svgMapRef() {
+            return _.filter(this.$children, function(o) { return (o.$el.className == "svg-map") })[0];
         }
       }
     });
