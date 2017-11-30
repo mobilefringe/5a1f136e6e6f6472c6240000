3f3ad5c6efe69922ae5d8c322806e1b580436129
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 11:11:13 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -31,29 +31,49 @@
 
 <script>
     define(["Vue", "vue!today_hours", "vue!search-component", 'vue!vue-slick'], function(Vue, TodayHoursComponent, SearchComponent, slick) {
-    return Vue.component("home-component", {
-      template: template, // the variable template will be injected
-      data: function() {
-        return {
-          title: "MM with Vue.js!",
-          description: "An example of integration of Mall Maverick with Vue.js",
-          suggestionAttribute: 'name',
-          search: ''
-        }
-      },
-      computed: {
-        property(){
-          return this.$store.getters.getProperty;
-        },
-        processedStores() {
-          return this.$store.getters.processedStores;
-        }
-      },
-      methods: {
-        onOptionSelect(option) {
-          console.log('Selected option:', option)
-        }
-      }
+        return Vue.component("home-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    title: "MM with Vue.js!",
+                    description: "An example of integration of Mall Maverick with Vue.js",
+                    suggestionAttribute: 'name',
+                    search: '',
+                    slickOptions: {
+                        slidesToShow: 1,
+                        dots : true,
+                        arrows:true,
+                        prevArrow: '.prev',
+                        nextArrow: '.next',
+                        autoplay: false
+                        // Any other options that can be got from plugin documentation
+                    }
+                }
+            },
+            computed: {
+                property(){
+                  return this.$store.getters.getProperty;
+                },
+                processedStores() {
+                  return this.$store.getters.processedStores;
+                },
+                banners () {
+                    console.log(_.orderBy(this.$store.state.results.banners, ['position'], ['asc']));
+                    return _.orderBy(this.$store.state.results.banners, ['position'], ['asc']);
+                },
+                banner_features () {
+                   return  _.slice(this.$store.state.results.feature_items, 0, 2);
+                },
+                feature_items () {
+                    console.log(this.$store.state.results.feature_items);
+                    return _.slice(this.$store.state.results.feature_items, 2,this.$store.state.results.feature_items.length );
+                }
+            },
+            methods: {
+                onOptionSelect(option) {
+                  console.log('Selected option:', option)
+                }
+            }
+        })
     })
-  })
 </script>
