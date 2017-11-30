c25dcb5199de90faf1d91abb7c10bcc5f848063e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 15:43:54 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -146,6 +146,9 @@
             created (){
                 window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
             },
+            mounted () {
+                
+            },
             methods: {
                 changeMode (mode) {
                     this.listMode = mode;
