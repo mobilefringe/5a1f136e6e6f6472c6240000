534d79ecc02864d33bf19f2e99f0380e85e3105f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 11:10:58 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -30,7 +30,7 @@
 </template>
 
 <script>
-  define(["Vue", "vue!today_hours", "vue!search-component"], function(Vue, TodayHoursComponent, SearchComponent) {
+    define(["Vue", "vue!today_hours", "vue!search-component", 'vue!vue-slick'], function(Vue, TodayHoursComponent, SearchComponent, slick) {
     return Vue.component("home-component", {
       template: template, // the variable template will be injected
       data: function() {
