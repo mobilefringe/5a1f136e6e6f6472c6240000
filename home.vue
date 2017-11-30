8c455fe4bce53a679621f634b65d13b8ced90e0a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-30 11:47:24 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -9,30 +9,29 @@
             </div>
         </slick>
         <div class="content_container main_container hidden_phone  position_relative">
-    <div class="feature_row" id="feature_container_2">
-        <script id="feature_template_2" type="x-tmpl-mustache/text">
-            <div class="feature_item_container">
-                <p class="feature_title">{{name}}</p>
-                <div class="feature_item wow animated flipInX animated teamy-team">
-                    <article class="teamy teamy_style2 teamy_mask-circle teamy_zoom-photo">
-                        <div class="teamy__layout">
-                            <a href="{{url}}" >
-                                <div class="teamy__preview">
-                                    <img src="{{image_url}}" class="teamy__avatar" alt="The demo photo">
-                                </div>
-                                <div class="teamy__back">
-                                    <div class="teamy__back-inner">
-                                        <div class="teamy__content">
-                                            <h3 class="teamy__name">View</h3>
+            <div class="feature_row" id="feature_container_2">
+                <div class="feature_item_container">
+                    <p class="feature_title">{{name}}</p>
+                    <div class="feature_item wow animated flipInX animated teamy-team">
+                        <article class="teamy teamy_style2 teamy_mask-circle teamy_zoom-photo">
+                            <div class="teamy__layout">
+                                <a href="{{url}}" >
+                                    <div class="teamy__preview">
+                                        <img src="{{image_url}}" class="teamy__avatar" alt="The demo photo">
+                                    </div>
+                                    <div class="teamy__back">
+                                        <div class="teamy__back-inner">
+                                            <div class="teamy__content">
+                                                <h3 class="teamy__name">View</h3>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
-                    </article>
+                                </a>
+                            </div>
+                        </article>
+                    </div>
                 </div>
             </div>
-        </script>
         </div>
     </div>
 </template>
