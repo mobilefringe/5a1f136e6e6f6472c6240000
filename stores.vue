858afa5452864c37d9e595b6369781647ec57a13
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 15:40:48 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -156,6 +156,7 @@
                     var div_stores = _.ceil(this.allStores.length/ 3);
                     var chunks = _.chunk(this.allStores,div_stores);
                     console.log(chunks);
+                    var num_store = 0;
                     _.forEach(storesByAlphaIndex, function(value, i) {
                         // chunks[i]=_.groupBy(value, store => (isNaN(store.name.charAt(0)) ? store.name.charAt(0) : "#"));
                         value.count
