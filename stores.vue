b9c241db3aa10127398836a92c5dfebc35da377b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 13:10:13 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -32,12 +32,12 @@
                 <h5 class="category_header" style="display:none" id="cat_name_header">All</h5>
                 <div class="row" v-if="processedStores">
                     <div class="col-md-4 store_col_1" v-for="n in 3">
-                    <div v-for="(stores,key) in processedStores" v-if="_.keys(processedStores).length /3">
-                        <span class="store_initial" :data-initial="key">{{key}}</span>
-                        <div id="store_list_container" class="store_list" v-for="store in stores">
-                            <div class="store_list_content cats_row" :data-cat="store.cat_list">
-                                <p class="store_name"><router-link :to="'/stores/'+store.slug">{{store.name}}</router-link></p>
-                            </div>
+                        <div v-for="(stores,key) in processedStores" v-if="_.keys(processedStores).length /3">
+                            <span class="store_initial" :data-initial="key">{{key}}</span>
+                            <div id="store_list_container" class="store_list" v-for="store in stores">
+                                <div class="store_list_content cats_row" :data-cat="store.cat_list">
+                                    <p class="store_name"><router-link :to="'/stores/'+store.slug">{{store.name}}</router-link></p>
+                                </div>
                         </div>   
                         </div>
                     </div>
