09f31f010aaf0053662475c05fae101588d5d664
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 14:53:38 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -155,7 +155,7 @@
                 sliceAllStores(start,end){
                     var div_stores = _.ceil(this.allStores.length/ 3);
                     var chunks = _.chunk(this.storesByAlphaIndex,div_stores);
-                    
+                    console.log(chunks)
                     _.forEach(chunks, function(value, i) {
                         chunks[i]=_.groupBy(value, store => (isNaN(store.name.charAt(0)) ? store.name.charAt(0) : "#"));
                     });
