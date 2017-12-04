6a7c45ec61c57eb51937a18c81842010eb3acdc7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 14:48:46 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -31,7 +31,7 @@
             <div class="col-md-12">
                 <h5 class="category_header" style="display:none" id="cat_name_header">All</h5>
                 <div class="row" v-if="processedStores">
-                    <div class="col-md-4 store_col_1" v-for="n in 3">
+                    <div class="col-md-4 store_col_1" v-for="chunk in 3">
                     
                         <div v-for="(stores,key) in sliceAllStores(((n-1) * 7),(n*7))">
                             <span class="store_initial" :data-initial="key">{{key}}</span>
