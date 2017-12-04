a629bf8ea838a3ae832191595301efe2f0f8b25f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 13:12:47 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -32,7 +32,7 @@
                 <h5 class="category_header" style="display:none" id="cat_name_header">All</h5>
                 <div class="row" v-if="processedStores">
                     <div class="col-md-4 store_col_1" v-for="n in 3">
-                        <div v-for="(stores,key) in processedStores" v-if="_.indexOf(alphabet, key) > (_.keys(processedStores).length/3 * n-2) && _.indexOf(alphabet, key) < (_.keys(processedStores).length/3 * n) ">
+                        <div v-for="(stores,key) in processedStores" v-if="_.indexOf(alphabet, key) > (_.keys(processedStores).length/3 * n-1) && _.indexOf(alphabet, key) < (_.keys(processedStores).length/3 * n) ">
                             <span class="store_initial" :data-initial="key">{{key}}</span>
                             <div id="store_list_container" class="store_list" v-for="store in stores">
                                 <div class="store_list_content cats_row" :data-cat="store.cat_list">
