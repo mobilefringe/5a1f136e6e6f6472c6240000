6061c185b34e2705493bb34651e662cbfeeac5cf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 12:01:02 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -16,7 +16,7 @@
             <select id="mobile_alpha_list" class="form-control custom_form_control">
                 <option disabled="disabled" selected>Index</option>
                 <option value="7">#</option>
-                <option value="a" v-for="letter in alphabet"> {{letter}}</option>
+                <option :value="letter" v-for="letter in alphabet"> {{letter}}</option>
                 <option value="a">a</option>
                 <option value="b">b</option>
                 <option value="c">c</option>
