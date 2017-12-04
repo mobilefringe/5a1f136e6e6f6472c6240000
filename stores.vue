2e18af6f195ecdfa4fd18cb6c5ecf9e130249e12
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 14:46:57 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -130,23 +130,6 @@
                         this.processedStores = _.groupBy(filtered, store => (isNaN(store.name.charAt(0)) ? store.name.charAt(0) : "#"));
                     }
                     
-                },
-                sliceAllStores(start,end){
-                    var div_stores = _.ceil(this.allStores.length/ 3);
-                    var chunks = _.chunk(this.allStores,div_stores);
-                    
-                    _.forEach(chunks, function(value, i) {
-                        chunks[i]=_.groupBy(value, store => (isNaN(store.name.charAt(0)) ? store.name.charAt(0) : "#"));
-                    });
-                    
-                    // console.log(chunks);
-                    // if (end >=20){
-                    //     end = this.alphabet.length;
-                    // }
-                    // var x =_.pick(this.storesByAlphaIndex, _.slice(this.alphabet, start, end));
-                    // // console.log(start,end);
-                    // // console.log(x);
-                   return chunks;
                 }
             },
             computed: {
