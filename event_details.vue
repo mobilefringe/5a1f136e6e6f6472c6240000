feb70c73e491f9c374fa50649f41bc5e62c75696
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:44:26 -0500

    Canyon Crest 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -82,7 +82,7 @@
             var vm = this;
             var temp_promo = [];
             var current_id =_.toNumber(this.currentEvent.id);
-            console.log(current_id);
+            console.log(current_id, this.allEvents);
             _.forEach(this.allEvents, function(value, key) {
                 if(_.toNumber(value) != current_id){
                     var current_promo = vm.findEventById(value);
