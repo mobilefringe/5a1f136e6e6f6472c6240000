cd6ba04f6b4cd98215db36307889f787ddec3735
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:47:37 -0500

    Canyon Crest 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -26,7 +26,7 @@
                 </div>
             </div>
         </div>
-        <div class="promo_main_header sub_title" v-if="storePromos" style="border-top: 1px solid #000;">
+        <div class="promo_main_header sub_title" v-if="storePromos && property" style="border-top: 1px solid #000;">
                OTHER {{property.name | uppercase }} PROMOTIONS
         </div>
         <div id="promos_container" v-if="storePromos">
