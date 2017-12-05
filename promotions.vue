3b83692f78b2b8046aea268c52b49b7a3e02d7f2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:20:59 -0500

    Canyon Crest 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -10,10 +10,10 @@
                                 
                             <!--</a>-->
                         </div>
-                            <p class="sub_title">{{ promo.store.name }}</p>
-                            <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
-                            <p class="description_text">{{ promo.name }}</p>
-                             <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="newsletter_btn animated_btn text_center">Read More</router-link>
+                        <p class="sub_title">{{ promo.store.name }}</p>
+                        <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+                        <p class="description_text">{{ promo.name }}</p>
+                        <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="newsletter_btn animated_btn text_center">Read More</router-link>
                         
                     </div>
                 </div>
