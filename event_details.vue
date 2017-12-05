8af9ee65ed150a9ab8f85ad013671eb871040f62
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:53:29 -0500

    Canyon Crest 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -53,7 +53,7 @@
 
 <script>
   define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
-    return Vue.component("promo-details-component", {
+    return Vue.component("event-details-component", {
       template: template, // the variable template will be injected,
       data: function() {
         return {
