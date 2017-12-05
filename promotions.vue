3376c09b82a0613deb766391e6ea91c3341f0096
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:23:40 -0500

    Canyon Crest 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -3,7 +3,7 @@
         <div class="page_title"> Promotions </div>
         <div class="row">
             <div class="col-md-4" v-for="promo in promotions">
-                <div class="promo_list_container">
+                <div class="promo_list_container text_center">
                     <div class="promo_list_img_container">
                         <!--<a :href="promo.image_url" target="_blank">-->
                         <img :src="promo.image_url" class="promo_list_img">
