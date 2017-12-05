af365b6b9cf020c1896cc0ff1ef61636dabe53b0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 12:44:54 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ require.config({
     'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min',
     'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min',
     'moment-timezone': 'https://momentjs.com/downloads/moment-timezone-with-data-2012-2022.min',
-    'vue2-filters': 'https://cdn.jsdelivr.net/npm/vue2-filters/dist/vue2-filters.min',
+    'vue2-filters': 'https://cdn.jsdelivr.net/vue2-filters/0.1.8/vue2-filters.min',
     'vue': 'https://mmvue.codecloudapp.com/require-vuejs.min',
     'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/2.3.1/vuex.min',
     'vue-i18n': 'https://cdnjs.cloudflare.com/ajax/libs/vue-i18n/6.1.1/vue-i18n.min',
