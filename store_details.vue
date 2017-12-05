b524a4b8c1c4259e5c585bcfafaf60c3a3202e01
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 11:14:34 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -129,6 +129,9 @@
                     this.$router.replace({ name: '404'});
                 }
             },
+            created (){
+                window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
+            },
             watch : {
                 map : function (){
                     setTimeout(function () {
