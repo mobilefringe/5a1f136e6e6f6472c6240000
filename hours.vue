226f1110d40e02144fcc76dcc240bb8a827a1b2d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 18:18:12 -0500

    Canyon Crest 1.0  /hours.vue

---
+++ hours.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class=" main_container" id="promotions_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <h3 class="promotion_heading">Up Coming Events & Promotions!</h3>
+        <p class="exclusive_deals sub_title">Don’t miss our exclusive deals & events</p>
+        <div class="row">
+            <div class="col-md-4" v-for="promo in promotions">
+                <div class="promo_list_container text_center">
+                    <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+                    <div class="promo_list_img_container">
+                        <!--<a :href="promo.image_url" target="_blank">-->
+                        <img :src="promo.store.image_url" class="promo_list_img">
+                            
+                        <!--</a>-->
+                    </div>
+                    <p class="description_text">{{ promo.name }}</p>
+                    <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="animated_btn text_center">Read More</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+  .center{
+    text-align: center
+  }
+  .store-section a{
+    color: #708090;
+  }
+</style>
+
+<script>
+  define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
+    Vue.use(Meta);
+    return Vue.component("promos-component", {
+      template: template, // the variable template will be injected
+      computed: {
+        promotions() {
+          return this.$store.getters.processedPromos;
+        },
+        timezone () {
+          return this.$store.getters.getTimezone;
+        }
+      }
+    });
+  });
+</script>
