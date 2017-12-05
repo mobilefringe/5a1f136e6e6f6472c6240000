4f9b9d75262071f1072bab26bdfe1cefa17e1ffe
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:21:31 -0500

    Canyon Crest 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="container main_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="page_title"> Promotions </div>
-            <div class="row">
-                <div class="col-md-3" v-for="promo in promotions">
-                    <div class="promo_list_container">
-                        <div class="promo_list_img_container">
-                            <!--<a :href="promo.image_url" target="_blank">-->
-                            <img :src="promo.image_url" class="promo_list_img">
-                                
-                            <!--</a>-->
-                        </div>
-                        <p class="sub_title">{{ promo.store.name }}</p>
-                        <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
-                        <p class="description_text">{{ promo.name }}</p>
-                        <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="animated_btn text_center">Read More</router-link>
+        <div class="row">
+            <div class="col-md-3" v-for="promo in promotions">
+                <div class="promo_list_container">
+                    <div class="promo_list_img_container">
+                        <!--<a :href="promo.image_url" target="_blank">-->
+                        <img :src="promo.image_url" class="promo_list_img">
+                            
+                        <!--</a>-->
                     </div>
+                    <p class="sub_title">{{ promo.store.name }}</p>
+                    <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+                    <p class="description_text">{{ promo.name }}</p>
+                    <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="animated_btn text_center">Read More</router-link>
                 </div>
             </div>
+        </div>
     </div>
 </template>
 
