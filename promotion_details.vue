f4ee50d04f717984edf7e7bbcdeccb3942771958
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 16:59:59 -0500

    Canyon Crest 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -58,27 +58,27 @@
         }
       },
       watch : {
-        currentStore : function (){
-                    console.log("currentStore promo",this.currentStore );
-                    var vm = this;
-                    var temp_promo = [];
-                    var temp_job = [];
-                    _.forEach(this.currentStore.promotions, function(value, key) {
-                        // console.log(vm.findPromoById(value));
-                        var current_promo = vm.findPromoById(value);
-                        current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
-                        temp_promo.push(current_promo);
-                    });
-                    _.forEach(this.currentStore.jobs, function(value, key) {
-                        var current_job = vm.findJobById(value);
-                        current_job.description_short = _.truncate(current_job.description, {'length': 70});
-                        temp_job.push(current_job);
-                        
-                    })
-                    this.promotions = temp_promo;
-                    this.jobs = temp_job;
-                    console.log("promos",this.promotions);
-                }  
+        currentPromo : function (){
+            console.log("currentStore promo",this.currentStore );
+            var vm = this;
+            var temp_promo = [];
+            var temp_job = [];
+            _.forEach(this.currentStore.promotions, function(value, key) {
+                // console.log(vm.findPromoById(value));
+                var current_promo = vm.findPromoById(value);
+                current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
+                temp_promo.push(current_promo);
+            });
+            _.forEach(this.currentStore.jobs, function(value, key) {
+                var current_job = vm.findJobById(value);
+                current_job.description_short = _.truncate(current_job.description, {'length': 70});
+                temp_job.push(current_job);
+                
+            })
+            this.promotions = temp_promo;
+            this.jobs = temp_job;
+            console.log("promos",this.promotions);
+        }  
       },
       computed: {
         findPromoBySlug () {
