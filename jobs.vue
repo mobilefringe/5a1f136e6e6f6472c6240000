ed6abb6b8403a1a5c32d7d2198eddab5bee91208
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:58:06 -0500

    Canyon Crest 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -4,24 +4,24 @@
         <p class="exclusive_deals sub_title">Join our family of dedicated professionals!</p>
         <div class="row">
             <div id="promos_container">
-                    <div class="col-md-6 no_padding" v-for="promo in promotions" :data-cat="promo.cat_list">
-                        <div class="promo_item cats_row is-table-row">
-                            <div class="col-md-5 no_padding">
-                                <img class="promo_store_image" :src="promo.image_url" :alt="promo.name" />
-                            </div>
-                            <div class="col-md-7 padding_tb_20">
-                                <h2 class="promo_list_name">{{promo.name}}</h2>
-                                <p>
-                                    <span class="promo_dates sub_title">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</span>
-                                </p>
-                                <div class="promo_list_desc">{{promo.description_short }}</div>
-                                <div class="text_center position_relative">
-                                    <router-link :to="'/promotions/'+promo.slug" class="animated_btn text_center">Read More</router-link>
-                                </div>
+                <div class="col-md-6 no_padding" v-for="promo in promotions" :data-cat="promo.cat_list">
+                    <div class="promo_item cats_row is-table-row">
+                        <div class="col-md-5 no_padding">
+                            <img class="promo_store_image" :src="promo.image_url" :alt="promo.name" />
+                        </div>
+                        <div class="col-md-7 padding_tb_20">
+                            <h2 class="promo_list_name">{{promo.name}}</h2>
+                            <p>
+                                <span class="promo_dates sub_title">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</span>
+                            </p>
+                            <div class="promo_list_desc">{{promo.description_short }}</div>
+                            <div class="text_center position_relative">
+                                <router-link :to="'/promotions/'+promo.slug" class="animated_btn text_center">Read More</router-link>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
     
