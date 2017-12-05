307d0aa9c0ce93dc74d4a1bec0faed8dd8f0d288
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 11:23:17 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -61,7 +61,7 @@
                                 <span class="promo_dates">{{dates}}</span>
                             </p>
                             <div class="promo_list_desc">{{promo.description_short}}</div>
-                            <a class="read_more" href="/promotions/{{slug}}">Read More...</a>
+                            <router-link class="read_more" :to="/promotions/{{slug}}">Read More...</router-link>
                         </div>
                     </div>
                 </div>
