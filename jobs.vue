00a8ba41382d24b86da765c222976f9d245236a5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:58:33 -0500

    Canyon Crest 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -7,7 +7,7 @@
                 <div class="col-md-6 no_padding" v-for="promo in jobs" :data-cat="promo.cat_list">
                     <div class="promo_item cats_row is-table-row">
                         <div class="col-md-5 no_padding">
-                            <img class="promo_store_image" :src="promo.image_url" :alt="promo.name" />
+                            <img class="promo_store_image" :src="promo.store.image_url" :alt="promo.name" />
                         </div>
                         <div class="col-md-7 padding_tb_20">
                             <h2 class="promo_list_name">{{promo.name}}</h2>
