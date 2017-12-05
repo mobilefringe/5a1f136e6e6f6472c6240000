0092535ac8d6ef7a90fecc2b4b42959ef975a71d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 17:46:28 -0500

    Canyon Crest 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row main_container" v-if="currentEvent">
-        <div class="promo_main_header sub_title">
+        <div class="promo_main_header sub_title" v-if="property">
                 {{property.name | uppercase}}
         </div>
         <div class="row mobile_padding" id="promo_details_container">
