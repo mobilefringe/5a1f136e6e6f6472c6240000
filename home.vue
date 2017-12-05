8db9028d0be483923ed99371da2c1fbc353a61c0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 14:28:37 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -61,7 +61,7 @@
                 <div class="newsletter_container text-center">
                         <h2 class="heading">SIGN UP TO RECEIVE OUR UPDATES</h2>
                         <p class="newsletter_desc sub_title">Special Offers <br class="show_phone" /><span>•</span> New Store Openings <br class="show_phone" /><span>•</span> Exclusive Events</p>
-                        <router-link to="/newsletter" class="animated_btn newsletter__btn">Subscribe</router-link>
+                        <router-link to="/newsletter" class="animated_btn newsletter_btn">Subscribe</router-link>
                     </div>
                  <hr/>
             </div>
