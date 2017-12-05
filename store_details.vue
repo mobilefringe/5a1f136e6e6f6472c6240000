5ce4a0beaeed5508f2471ddd26d2197d3d62a874
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 11:21:29 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -98,6 +98,7 @@
                 return {
                     currentStore: null,
                     promotions : [],
+                    jobs:[],
                     map: null
                 }
             },
