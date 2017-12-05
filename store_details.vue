cce1fe95bdb77e8848d3a5aa331e04999ce56ebe
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-05 12:11:10 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -56,7 +56,7 @@
                     {{currentStore.name}} Promotions
                 </h2>
                 <div id="promos_container">
-                    <div class="row promo_item cats_row" v-for="promo in promotions" :data-cat="promo.cat_list">
+                    <div class="col-md-6 promo_item cats_row" v-for="promo in promotions" :data-cat="promo.cat_list">
                         <div class="col-md-5">
                             <img class="promo_store_image" :src="promo.image_url" :alt="promo.name" />
                         </div>
