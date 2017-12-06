a26a6cbb63b78c7ee60cbe2a2e74e421ad1841a3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 12:13:26 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -75,7 +75,7 @@
                 })
             },
             beforeRouteUpdate (to, from, next) {
-                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/contact.json"}).then(response => {
                     // this.dataLoaded = true;
                     this.currentPage = response.data;
                     console.log(this.currentPage);
