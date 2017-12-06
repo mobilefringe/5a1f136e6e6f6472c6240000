f35b634bc6e17bf9d07c2d7750c2feb614c97a8a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 11:40:43 -0500

    Canyon Crest 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -26,8 +26,8 @@
                             <h3 class="hours_heading" >Regular Shopping Hours</h3>
                             <div id="hours_container" class="hours_container">
                                     <div class="hours_div" v-for="hour in hours">
-                                        <span>{{day}}:</span>
-                                        <span>{{h}}</span>
+                                        <span>{{day}}:</span>("h:mm A")
+                                        <span>{{promo.start_date | moment("h:mm A", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</span>
                                     </div>
                             </div>
                             <div class="padding_top_20"></div>
