85c39c91fa050a6ee2fc83f6ad71f0d6d4ffeb6e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 12:37:30 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -47,7 +47,7 @@
                         </div>
                         <div class="col-sm-12 col-md-12">
                         <br>
-                            <label for="enterVerify">Enter the following number below to proceed: <div id="verifyNum"></div></label><br>
+                            <label for="enterVerify">Enter the following number below to proceed: <div id="verifyNum">{{rannumber}}</div></label><br>
                             <input type="hidden" :value="rannumber" id="verifyNumHidden" name="verifyNumHidden" />
                             <input type="text" id="enterVerify" name="enterVerify" />
                         </div>
