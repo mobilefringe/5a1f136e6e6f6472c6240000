01c0b07a8ea5a2c14013742aed5fd061b5eaa54c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 12:12:32 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -59,31 +59,31 @@
                         "Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
                     ],
                     currentPage: null
-        }
-      },
-      beforeRouteEnter (to, from, next) {
-        next(vm => {
-          // access to component instance via `vm`
-            vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
-                
-                vm.currentPage = response.data;
-                console.log(vm.currentPage);
-            }, error => {
-                console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                vm.$router.replace({ name: '404'});
-            });
-        })
-      },
-      beforeRouteUpdate (to, from, next) {
-            this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
-                // this.dataLoaded = true;
-                this.currentPage = response.data;
-                console.log(this.currentPage);
-            }, error => {
-                console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                this.$router.replace({ name: '404'});
-            });
-      },
+                }
+              },
+              beforeRouteEnter (to, from, next) {
+                next(vm => {
+                  // access to component instance via `vm`
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                        
+                        vm.currentPage = response.data;
+                        console.log(vm.currentPage);
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        vm.$router.replace({ name: '404'});
+                    });
+                })
+              },
+            beforeRouteUpdate (to, from, next) {
+                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                    // this.dataLoaded = true;
+                    this.currentPage = response.data;
+                    console.log(this.currentPage);
+                }, error => {
+                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                    this.$router.replace({ name: '404'});
+                });
+            },
             mounted () {
               console.log(this.hours);
               console.log(this.holidayHours);
