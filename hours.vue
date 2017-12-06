a6ca0698c4c640a36b7db4547c84508032b47be1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 11:46:41 -0500

    Canyon Crest 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -42,7 +42,7 @@
                             <h3 class="hours_heading">Holiday Closures</h3>
                             <div id="closed_hours_container" class="hours_container">
                                     <div class="hours_div" v-for="hour in holidayHours">
-                                        <span>{{holiday_name}} ({{formatted_date}})</span>
+                                        <span>{{hour.holiday_name}} ({{formatted_date}})</span>
                                     </div>
                             </div>
                         </div>
