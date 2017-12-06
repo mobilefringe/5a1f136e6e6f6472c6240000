776343515712cdcc865f6fdd35e8ae3b243c46aa
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 12:41:57 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -4,9 +4,7 @@
             <div class="row">
                 <div class="col-md-5">
                     <div class="col-md-12" v-if="currentPage">
-                        <!--<div class="col-md-12"> -->
-                            <div class="description_text text_left" v-html="currentPage.body"></div>
-                        <!--</div>-->
+                        <div class="description_text text_left" v-html="currentPage.body"></div>
                     </div>
                 </div>
                 <div class="col-md-7 hidden_phone">
