619e30b095659d49ee1b7eba7792ec4d3245b87e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 12:31:28 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -16,7 +16,7 @@
                 </div> 
             </div>
             <hr/>
-            <div class="col-md-8 contact_contents">
+            <div class="col-md-12 contact_contents">
             
                 <form name="contact_form" id="contact_form" method="post">
                     <div class="row "  style="margin-left:0px;margin-right:0px">
