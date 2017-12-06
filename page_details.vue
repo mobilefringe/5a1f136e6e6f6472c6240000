c4436b0ab704820a79ae74239eb5923b3e0aa5b6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 13:32:45 -0500

    Canyon Crest 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -2,7 +2,7 @@
     <div>
          <div class="main_container " v-if="currentPage">
             <h4 class="page_title promo_main_header" v-html="currentPage.title">  </h4>
-            <div class="row"padding_30>
+            <div class="row padding_30" >
                 <div class="description_text text_left" v-html="currentPage.body">
                     
                 </div>
