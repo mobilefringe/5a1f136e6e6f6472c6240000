8fa6dff7709a402fd1c66becce60bf633e25b965
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 15:26:36 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -129,7 +129,7 @@
                     var cat_id = null;
                     if(this.selected_cat == "All")
                     if(this.selected_cat == "All"){
-                        cat_id = this.selected_cat;
+                        cat_id = "all";
                     }
                     else {
                         cat_id= this.findCategoryByName(this.selected_cat).id;
