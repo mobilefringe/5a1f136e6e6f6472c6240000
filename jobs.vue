db818a64d1b67a9c0c3cf2dc2143eae1181d36ea
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 17:12:14 -0500

    Canyon Crest 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -20,7 +20,7 @@
                                 <span class="promo_dates sub_title">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</span>
                             </p>
                             <div class="promo_list_desc hidden_phone">{{truncate(promo.description) }}</div>
-                            <div class="text_center position_relative">
+                            <div class="text_center position_relative hidden_phone">
                                 <router-link :to="'/jobs/'+promo.slug" class="animated_btn text_center">Read More</router-link>
                             </div>
                         </div>
