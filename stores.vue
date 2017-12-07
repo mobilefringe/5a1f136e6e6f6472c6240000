fbacd4a3f8772ccb6e1618496f73b70fbbedc9bf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 11:31:43 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -29,11 +29,11 @@
                 <!--<select id="mobile_cat_list" class="form-control custom_form_control">-->
                 <!--        <option value="{{id}}">{{name}}</option>-->
                 <!--</select>-->
-                <select id="mobile_alpha_list" class="form-control custom_form_control">
-                    <option disabled="disabled" selected>Index</option>
-                    <option value="7">#</option>
-                    <option v-for="letter in alphabet" :value="letter"> {{letter}}</option>
-                </select>
+                <!--<select id="mobile_alpha_list" class="form-control custom_form_control">-->
+                <!--    <option disabled="disabled" selected>Index</option>-->
+                <!--    <option value="7">#</option>-->
+                <!--    <option v-for="letter in alphabet" :value="letter"> {{letter}}</option>-->
+                <!--</select>-->
                 <v-select v-model="selected_cat" :options="allCatergories" :searchable="false" class="open" :on-change="consoleCallback" label="name"></v-select> 
             </div>
             <div class="row padding_bottom_50">
