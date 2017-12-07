451a3be018afa0903057cc9a5fb063e7a51bc51f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 14:29:20 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -37,7 +37,7 @@
                 <div class="select_container">
                     <v-select :value="selected_cat" :options="mobile_aphabet" :searchable="false" :on-change="filteredByCategory" id="mobile_cat_list"></v-select> 
                 </div>
-                <div>
+                <div class="select_container">
                     <v-select :value="selected_alpha" :options="mobile_aphabet" :searchable="false" :on-change="filterStores" id="mobile_alpha_list"></v-select> 
                 </div>
                 
