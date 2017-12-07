d60453ad721d8472bcb77524d49ef53d5de0ae54
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 11:31:12 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -34,7 +34,7 @@
                     <option value="7">#</option>
                     <option v-for="letter in alphabet" :value="letter"> {{letter}}</option>
                 </select>
-                
+                <v-select v-model="selected_cat" :options="allCatergories" :searchable="false" class="open" :on-change="consoleCallback" label="name"></v-select> 
             </div>
             <div class="row padding_bottom_50">
                 <div class="col-md-12">
