e0865b6bbb78552738bae2185bbf5217a4d3630e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 17:04:24 -0500

    Canyon Crest 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -38,7 +38,9 @@
                         <img class="promo_store_image" :src="promo.image_url" :alt="promo.name" />
                     </div>
                     <div class="col-md-7 padding_tb_20">
-                        <h2 class="promo_list_name">{{promo.name}}</h2>
+                        <router-link :to="'/promotions/'+promo.slug" class="">
+                            <h2 class="promo_list_name">{{promo.name}}</h2>
+                        </router-link>
                         <p>
                             <span class="promo_dates sub_title">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</span>
                         </p>
