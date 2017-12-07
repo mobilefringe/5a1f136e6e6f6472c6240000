1b2fbc9079b97f1940cf2adaa81356299b489200
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 17:23:51 -0500

    Canyon Crest 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -2,7 +2,7 @@
     <div class=" main_container" id="promotions_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="margin_25_across padding_top_40">
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-5 col-xs-12">
                     <div class="row">
                         <div class="col-md-12"> 
                             <h3 class="hours_heading" >Regular Shopping Hours</h3>
