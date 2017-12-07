12f10de2a04b06dd1ac0ea4fae2223c031c2e64a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-07 16:28:10 -0500

    Canyon Crest 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -3,7 +3,6 @@
         <h3 class="promotion_heading">Up Coming Events & Promotions!</h3>
         <p class="exclusive_deals sub_title">Don’t miss our exclusive deals & events</p>
         <div class="row">
-        <hr class="show_phone">
             <div class="col-md-4" v-for="promo in promotions">
                 <div class="promo_list_container text_center">
                     <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
