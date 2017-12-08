891525e76d833f78afa8f0bf5ec41c776f0b608c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-08 10:21:17 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -78,7 +78,16 @@
                           <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
                         </div>
                       </div>
-                    
+                        
+                        <div class="form-group">
+                        <div class="col-xs-6" :class="{'has-error': errors.has('phone')}">
+                            <label class="label" for="email">Phone</label>
+                          <!-- <input class="form-control" type="email" required="" placeholder="Email"> -->
+                          <input v-model="user.phone" v-validate="'required|phone'" class="form-control" :class="{'input': true}" name="phone" type="phone"
+                            placeholder="Phone" data-vv-delay="1000">
+                          <span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
+                        </div>
+                      </div>
                       <div class="form-group">
                         <div class="col-xs-6" :class="{'has-error': errors.has('phone')}">
                             <label class="label" for="email">Phone</label>
