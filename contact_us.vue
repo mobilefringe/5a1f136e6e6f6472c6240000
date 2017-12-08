30b6e9d24b35d52cd599a3e9cfa477b23dbad0b5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-08 10:16:28 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -63,7 +63,7 @@
                     <!--</form>-->
                     <form class="form-horizontal" action="form-submit" @submit.prevent="validateBeforeSubmit">
                       <div class="form-group ">
-                        <div class="col-xs-6" :class="{'has-error': errors.has('email')}">
+                        <div class="col-xs-6" :class="{'has-error': errors.has('name')}">
                             <label class="label" for="email">Email</label>
                           <!-- <input class="form-control" type="email" required="" placeholder="Email"> -->
                           <input v-model="user.username" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email"
