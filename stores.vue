3b16c8ca18dd3f342577e779802198b7a3d06b23
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-08 12:54:31 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -180,7 +180,7 @@
                     return this.$store.getters.getProperty;
                 },
                 getSVGurl () {
-                    // return "https://www.mallmaverick.com" + this.property.svgmap_url;
+                    return "https://www.mallmaverick.com" + this.property.svgmap_url;
                     return "//www.mallmaverick.com/system/site_images/photos/000/035/014/original/Canyon_Crest_-_Map.svg?1512066588";
                 },
                 allStores() {
