73a3f0a5bfa1c048888b7df3f0659912f7eefb15
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-08 12:35:59 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -90,8 +90,8 @@
                         <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
                             <label class="label" for="message">Message</label>
                           <!-- <input class="form-control" type="email" required="" placeholder="Email"> -->
-                          <input v-model="form_data.message" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="message" type="text"
-                            placeholder="Message" data-vv-delay="1000">
+                          <text-area v-model="form_data.message" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="message" type="text"
+                            placeholder="Message" data-vv-delay="1000"></text-area>
                           <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
                         </div>
                       </div>
