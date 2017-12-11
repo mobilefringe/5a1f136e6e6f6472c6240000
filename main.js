4d22f740ff8496f5d80b32166476f80be21ee924
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-11 16:26:29 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -29,7 +29,8 @@ require.config({
     'mm_mapsvg': 'https://mmvue.codecloudapp.com/mallmaverick_svgmap',
     'svg-map': 'https://5a1f136e6e6f6472c6240000.codecloudapp.com/svg_map.vue?noext',
     'vue-social-sharing': 'https://cdn.jsdelivr.net/npm/vue-social-sharing@2.3.1/dist/vue-social-sharing.min',
-    'vee-validate' : 'https://cdn.jsdelivr.net/npm/vee-validate@latest/dist/vee-validate'
+    'vee-validate' : 'https://cdn.jsdelivr.net/npm/vee-validate@latest/dist/vee-validate',
+    '' : 'https://cdn.jsdelivr.net/npm/vue-datepicker-local@1.0.9/dist/vue-datepicker-local'
   }
 });
 
