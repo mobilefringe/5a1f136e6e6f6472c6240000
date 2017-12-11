8502cc68f6293628c5565343ccd8ec22164a2b4d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-11 16:34:11 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
   });
 
 Vue.component('v-select', VueSelect.VueSelect);
-Vue.component('vue-datepicker-local', DatepickerLocal);
+// Vue.component('vue-datepicker-local', DatepickerLocal);
 
   /* bootstrap app */
   const vm = new Vue({
