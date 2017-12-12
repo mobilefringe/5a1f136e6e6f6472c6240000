4e4ac3e1ef6b6bf6b24dbe78a19790c8c0d183e8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-12 11:28:34 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -205,8 +205,8 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             this.desktop_search = "";
         },
         enableStickyNavbar (e) {
-            console.log("scrolling");
-            console.log(window.pageYOffset,this.isSticky,window.pageYOffset >= this.isSticky );
+            // console.log("scrolling");
+            // console.log(window.pageYOffset,this.isSticky,window.pageYOffset >= this.isSticky );
             if (window.pageYOffset >= this.isSticky) {
                 this.stickyMenu = true;
                 // this.navbar.classList.add("sticky")
