6f4de2d9cbbd7a446b32c6221a0c5b329b33b901
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-14 14:37:41 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -153,7 +153,6 @@
                             console.log("sending form data", this.form_data);
                             send_data = {};
                             send_data.url = '/api/v1/contact_us';
-                            let formData =
                             send_data.form_data = JSON.stringify(this.form_data);
                             this.$store.dispatch("CONTACT_US", send_data).then(res => {
                                 // this.$router.replace({
