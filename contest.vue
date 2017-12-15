3195c0020f1d2e7d56b9aaf4e371f0a633fd45d2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-15 17:20:59 -0500

    Canyon Crest 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -36,8 +36,8 @@
                         <input v-model="form_data.phone" v-validate="'required|alpha_dash'" class="form-control" :class="{'input': true}" name="phone" type="phone" placeholder="Phone" data-vv-delay="1000">
                         <span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
                     </div>
-                    <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('name')}">
-                        <label class="label" for="subject">Subject</label>
+                    <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('postal_code')}">
+                        <label class="label" for="postal_code">Postal Code</label>
                         <input v-model="form_data.subject" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="subject" type="text" placeholder="Subject" data-vv-delay="1000">
                         <span v-show="errors.has('subject')" class="form-control-feedback">{{ errors.first('subject') }}</span>
                     </div>
