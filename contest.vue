782d836423cae0dcb60655f1ae3e8c76f2daa86f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-15 17:07:08 -0500

    Canyon Crest 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -57,6 +57,16 @@
                             </div>
                         </div>
                     </form>
+                    <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">Success</span>
+                        Thank you for contacting us. A member from our team will contact you shortly.
+                    </div>
+                    <div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <span class="sr-only">Error:</span>
+                        There was an error when trying to submit your request. Please try again later.
+                    </div>
         </div>
         <div id="events_empty_section" v-show="currentContest">
             <p>Check back later for exciting contest details!</p>
