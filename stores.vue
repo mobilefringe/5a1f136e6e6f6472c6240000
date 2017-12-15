a1f0a6683c2c2dea0d2763e3b50cd5263f1350b9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-15 14:02:53 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -59,7 +59,7 @@
                 <div class="col-md-12">
                     <h5 class="category_header" style="display:none" id="cat_name_header">All</h5>
                     <div class="row" v-if="processedStores">
-                        <div class="col-md-12 store_col_1" id="{ storelist_container: break_into_col }">
+                        <div class="'col-md-12 store_col_1' + { storelist_container: break_into_col }">
                             <div v-for="(stores,key) in processedStores" style="padding:0 10px;">
                                 <span class="store_initial" :data-initial="key">{{key}}</span>
                                 <div id="store_list_container" class="store_list" v-for="store in stores">
