14e611a887f7a205ca195ce85c7f453e410d1410
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-15 16:57:13 -0500

    Canyon Crest 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" main_container" id="events_container"><!-- for some reason if you do not put an outer container div this component template will not render -->
-        <h3 class="promotion_heading" v-if="property"> Exclusive Events at {{property.name}}</h3>
-        <p class="exclusive_deals sub_title">Join us to celebrate and enjoy!</p>
+        <!--<h3 class="promotion_heading" v-if="property"> Exclusive Events at {{property.name}}</h3>-->
+        <!--<p class="exclusive_deals sub_title">Join us to celebrate and enjoy!</p>-->
         <div class="row">
             <div class="promo-details">
                 <div class="event-image">
