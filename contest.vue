0008e9b82255a87edfe4fe04ed0dd49aa9df41eb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-15 17:13:17 -0500

    Canyon Crest 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -121,6 +121,7 @@
             },
             mounted () {
                 this.currentContest = this.findContestBySlug('canyoncrest-test-contest');
+                console.log(currentContest);
             },
             // watch : {
             //     currentContest : function (){
