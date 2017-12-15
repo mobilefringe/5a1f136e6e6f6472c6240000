bd3afac6dc7b3ee9204656268df925f722f7b7c6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-15 17:18:31 -0500

    Canyon Crest 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -16,7 +16,7 @@
                 <div class="form-group ">
                     <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('name')}">
                         <label class="label" for="first_name">First Name</label>
-                        <input v-model="form_data.first_name" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="first_name" type="text" placeholder="Name" data-vv-delay="1000">
+                        <input v-model="form_data.first_name" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="first_name" type="text" placeholder="First Name" data-vv-delay="1000">
                         <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
                     </div>
                     <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('email')}">
