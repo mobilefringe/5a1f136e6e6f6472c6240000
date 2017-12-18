12958c8d2c9bb0acd4acb9e896170afcc437e190
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-18 14:29:04 -0500

    Canyon Crest 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -53,7 +53,7 @@
       },
       computed: {
         allJobs() {
-            var jobs = this.$store.getters.processedJobs;
+            var jobs = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 });
             console.log(jobs);
             jobs = _.sortBy(jobs, [function(o) { if(o.store) return o.store.name; }]);
             // console.log(_.groupBy(jobs, job => job.store.name));
