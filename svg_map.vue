91b937b917f56ad93adfea97acc362694edba90b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 10:20:22 -0500

    Canyon Crest 1.0  /svg_map.vue

--- svg_map.vue
+++ svg_map.vue
@@ -132,7 +132,7 @@
                         var width = parseInt(coords["width"])
                         var x_offset = (parseInt(width) / 2);
                         var y_offset = (parseInt(height) /2);
-                        var mark = { xy: [coords["x"]    + x_offset-15, coords["y"]  + y_offset-30],
+                        var mark = { xy: [coords["x"]    + x_offset-15, coords["y"]  + y_offset-35],
                             attrs: {
                                 id:"marker_"+ val.id,
                                 src:image , // image for marker
