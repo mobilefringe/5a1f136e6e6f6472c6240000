de25f0b43be500f2b864f23d72445d9cb14cb592
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 12:53:45 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -206,7 +206,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             this.desktop_search = "";
         },
         currentlySearching (value) {
-            console.log('currentlySearching:', value, value.length);
+            console.log('currentlySearching:', value, value);
             if(value.size > 0){
                 this.is_searching = true;
             }
