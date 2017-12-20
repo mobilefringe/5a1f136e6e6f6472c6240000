8e7b5e0bb77d87262d9f36cbd8129441e3980fd2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 11:59:01 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
         desktop_search : null,
         suggestionAttribute: 'name',
         stickyMenu : false,
-        isSticky : 185,
+        isSticky : 200,
         navbar : null,
         menu_items: [
             // {
