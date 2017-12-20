5b7fa000664f91aca52e45bfaca4e9a8f38ef107
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 15:32:57 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -46,8 +46,8 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     mode: 'history',
     routes: appRoutes,
     scrollBehavior (to, from, savedPosition) {
-     return { x: 0, y: 0 }
-   }
+        return { x: 0, y: 0 }
+    }
   });
 
   /* initialize i18n */
