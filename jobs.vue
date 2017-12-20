7b4d0d6824903efe7c0cc718a5396002cc91b836
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 11:56:59 -0500

    Canyon Crest 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -10,7 +10,7 @@
                 <div class="col-md-6 no_padding" v-for="promo in jobs" :data-cat="promo.cat_list">
                     <div class="promo_item cats_row is-table-row" v-if="promo">
                         <div class="col-md-5 col-xs-4 no_padding">
-                            <img class="promo_store_image" v-if="promo.store.image_url" :src="promo.store.image_url" :alt="promo.name" />
+                            <img class="promo_store_image" v-if="promo.store" :src="promo.store.image_url" :alt="promo.name" />
                             <img class="promo_store_image" v-else src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1513356843893/canyon_crest_logo.png" :alt="promo.name" />
                         </div>
                         <div class="col-md-7  padding_tb_20">
