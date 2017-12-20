359bc14e014fd98ce5ddaf336ee1aba17b0bafc8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 14:10:32 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -150,6 +150,8 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             }
             console.log("Changed to route:",this.$route.path, "subscription is", this.show_subscrption);
             this.show_mobile_menu = false; //close menu when navigating to new page
+            
+            //clear search when changing routes
         },
         mobile_search : function () {
             // console.log(this.mobile_search);
