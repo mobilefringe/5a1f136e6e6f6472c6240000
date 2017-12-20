62e09cd0747664722e71a8da26a3fbb2a4b7c541
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 10:35:49 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -182,7 +182,7 @@
                         _.forEach(filtered, function(value, i) {
                             value.currentCategory = find(category_id).name;
                         });
-                        console.log(filtered)
+                        console.log(filtered);
                         sortedCats = _.groupBy(filtered, store => store.currentCategory);
                         console.log(sortedCats);
                         this.processedStores = sortedCats;
