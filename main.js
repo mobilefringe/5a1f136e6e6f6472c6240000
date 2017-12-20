749d7b888e4cb7ecca73e07e9872a29ee85732e4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 14:24:37 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -206,7 +206,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             console.log('Selected option:', option);
             
             console.log(this.is_searching);
-            this.$router.push("/stores/"+option.slug);
+            this.$router.push(path:{"/stores/"+option.slug});
             console.log(this.$router);
         },
         currentlySearching (value) {
