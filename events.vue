420cb1e35bbbd08795e27b3aba1249b3582193ee
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-21 12:58:48 -0500

    Canyon Crest 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -59,7 +59,7 @@
                 var temp_events = this.events;
                 temp_events.map(event => {
                     event.title = event.name;
-                    event.locale = state.locale;
+                    event.start = state.locale;
                     event.store = null;
                     if (event.eventable_type === "Store") {
                         let foundStore = stores.find(store => store.id === event.eventable_id.toString());
