44c83242b8836115674a63453091329d4eda88ec
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-21 15:48:06 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
 
     Vue.component('v-select', VueSelect.VueSelect);
     // Vue.component('full-calendar', FullCalendar)
-    Vue.component('full-calendar', FullCalendar)
+    Vue.component('full-calendar', VueFullCalendar)
   /* bootstrap app */
   const vm = new Vue({
     el: '#app',
