0ced2e1d339175960959d1482a3abba09bd60c0b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-21 12:28:46 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -35,10 +35,10 @@ require.config({
 });
 
 require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'locales','jquery', 'moment', "vue-meta", "moment-timezone", "vue-moment",'vue-social-sharing','vue-select', "vue!search-component", 'vue-fullcalendar'], function (Vue, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages,$, moment, Meta, tz, VueMoment,SocialSharing, VueSelect, SearchComponent, FullCalendar) {
-  Vue.use(Meta);
-  Vue.use(VueRouter);
-  Vue.use(Vue2Filters);
-  Vue.use(VueI18n);
+    Vue.use(Meta);
+    Vue.use(VueRouter);
+    Vue.use(Vue2Filters);
+    Vue.use(VueI18n);
     Vue.use(SocialSharing);
     // Vue.use(DatepickerLocal);
   /* initialize router */
