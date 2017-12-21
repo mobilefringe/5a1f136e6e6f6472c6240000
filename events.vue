fed6d6004b02c593e3f455cc7aa27c52f94729cf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-21 13:03:03 -0500

    Canyon Crest 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -59,7 +59,7 @@
                 var temp_events = this.events;
                 temp_events.map(event => {
                     event.title = event.name;
-                    event.start = moment(event.start_date).format("YYYY-MM-DD").tz(getPropertyTimeZone());
+                    event.start = moment(event.start_date).format("YYYY-MM-DD").tz(this.timezone);
                     event.end = moment(event.end_date).format("YYYY-MM-DD").tz(getPropertyTimeZone());
                     if (event.eventable_type === "Store") {
                         let foundStore = stores.find(store => store.id === event.eventable_id.toString());
