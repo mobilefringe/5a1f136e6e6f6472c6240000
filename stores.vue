3596349905387fdbed8b2b44aa9ec229af21caf7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 12:02:19 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -19,7 +19,7 @@
                     <div class="map_storelist">
                         <v-select :options="allStores" :placeholder="'Select A Store'" :searchable="false" :label="'name'" :on-change="dropPin"></v-select> 
                     </div>
-                        <svg-map v-bind:svgMapUrl="getSVGurl"></svg-map>
+                        <svg-map v-bind:svg-map-url="getSVGurl"></svg-map>
                     </div>
                 </div>
             </div>
