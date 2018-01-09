f4220801fa0b9a81a1c9a9cb7af71962d9acebaf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-09 16:42:45 -0500

    Canyon Crest 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -113,8 +113,7 @@
                     return this.$store.getters.findEventById;
                 },
                 allEvents() {
-                    return 
-                    var jobs =this.$store.getters.processedEvents;
+                    var events =this.$store.getters.processedEvents;
                     jobs.map(job => {
                             if(job.store != null && job.store != undefined && _.includes(job.store.image_url, 'missing'))
                                 job.store.image_url = "//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/jpeg/1515531874445/canyon_crest_default.jpg";
