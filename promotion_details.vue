902f8571d8fa6e32df015f01b5bd5b9aa0c00ed1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-09 16:37:55 -0500

    Canyon Crest 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -94,7 +94,7 @@
                                 var current_promo = vm.findPromoById(value);
                                 current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
                                 
-                                if(_.includes(this.currentPromo.store.image_url, 'missing'))
+                                if(_.includes(current_promo.store.image_url, 'missing'))
                                     this.currentPromo.store.image_url = "//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/jpeg/1515531874445/canyon_crest_default.jpg";
                                 temp_promo.push(current_promo);
                             }
