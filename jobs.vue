fea4f0f78db2d662ed548f9d94402f9949bf6ed9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-09 16:17:24 -0500

    Canyon Crest 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -58,7 +58,7 @@
             var jobs = this.$store.getters.processedJobs; //_.filter(this.$store.getters.processedJobs, function(o) { return o.store !=null && o.jobable_type === "Store" });
             jobs.map(job => {
                     if(job.store != null && job.store != undefined && _.includes(job.store.image_url, 'missing'))
-                    job.image_url = "https://mallmaverick.cdn.speedyrails.net" + store.store_front_url;
+                    job.store.image_url = "https://mallmaverick.cdn.speedyrails.net" + store.store_front_url;
                 });
             console.log(jobs);
             jobs = _.sortBy(jobs, [function(o) { if(o.store) return o.store.name; }]);
