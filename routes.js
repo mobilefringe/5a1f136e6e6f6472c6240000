e8eb651af57341af00b87c5a0ae06eb3b0ee8b61
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-13 22:09:40 -0500

    Canyon Crest 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -1,159 +1,142 @@
-define([], function () {
-  return [
-    {
-      path: '/',
-      component: view('home')
-    },
-    {
-      path: '/stores',
-      component: view('default'),
-      children: [
-        {
-          path: '',
-          component: view('stores'),
-          name: 'storeList'
+define([], function() {
+    return [{
+            path: '/',
+            component: view('home')
         },
         {
-          path: ':id',
-          component: view('store_details'),
-          name: 'storeDetails'
-        }
-      ]
-    },
-    {
-      path: '/promotions',
-      component: view('default'),
-      children: [
-        {
-          path: '',
-          component: view('promotions'),
-          name: 'promoList'
+            path: '/stores',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('stores'),
+                    name: 'storeList'
+                },
+                {
+                    path: ':id',
+                    component: view('store_details'),
+                    name: 'storeDetails'
+                }
+            ]
         },
         {
-          path: ':id',
-          component: view('promotion_details'),
-          name: 'promotionDetails'
-        }
-      ]
-    },
-    {
-      path: '/events',
-      component: view('default'),
-      children: [
-        {
-          path: '',
-          component: view('events'),
-          name: 'eventList'
+            path: '/promotions',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('promotions'),
+                    name: 'promoList'
+                },
+                {
+                    path: ':id',
+                    component: view('promotion_details'),
+                    name: 'promotionDetails'
+                }
+            ]
         },
         {
-          path: ':id',
-          component: view('event_details'),
-          name: 'eventDetails'
-        }
-      ]
-    },
-    {
-      path: '/jobs',
-      component: view('default'),
-      children: [
-        {
-          path: '',
-          component: view('jobs'),
-          name: 'jobList'
+            path: '/events',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('events'),
+                    name: 'eventList'
+                },
+                {
+                    path: ':id',
+                    component: view('event_details'),
+                    name: 'eventDetails'
+                }
+            ]
         },
         {
-          path: ':id',
-          component: view('job_details'),
-          name: 'jobDetails'
-        }
-      ]
-    },
-    {
-      path: '/map',
-      component: view('default'),
-      redirect: '/404',
-      children: [
+            path: '/jobs',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('jobs'),
+                    name: 'jobList'
+                },
+                {
+                    path: ':id',
+                    component: view('job_details'),
+                    name: 'jobDetails'
+                }
+            ]
+        },
         {
-          path: '',
-          component: view('map'),
-          name: 'map'
-        }
-      ]
-    },
-    {
-      path: '/hours',
-      component: view('default'),
-      children: [
+            path: '/map',
+            component: view('default'),
+            redirect: '/404',
+            children: [{
+                path: '',
+                component: view('map'),
+                name: 'map'
+            }]
+        },
         {
-          path: '',
-          component: view('hours'),
-          name: 'hourList'
-        }
-      ]
-    },
-    {
-      path: '/contest',
-      component: view('default'),
-      children: [
+            path: '/hours',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('hours'),
+                name: 'hourList'
+            }]
+        },
         {
-          path: '',
-          component: view('contest'),
-          name: 'contestList'
-        }
-      ]
-    },
-    {
-      path: '/contact_us',
-      component: view('default'),
-      children: [
+            path: '/contest',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('contest'),
+                name: 'contestList'
+            }]
+        },
         {
-          path: '',
-          component: view('contact_us'),
-          name: 'contactUs'
-        }
-      ]
-    },
-    {
-      path: '/newsletter',
-      component: view('default'),
-      children: [
+            path: '/contact_us',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('contact_us'),
+                name: 'contactUs'
+            }]
+        },
         {
-          path: '',
-          component: view('newsletter'),
-          name: 'newsletter'
-        }
-      ]
-    },
-    {
-      path: '/pages',
-      component: view('default'),
-      redirect: '/404',
-      children: [
+            path: '/newsletter',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('newsletter'),
+                name: 'newsletter'
+            }]
+        },
         {
-          path: ':id',
-          component: view('page_details'),
-          name: 'pageDetails'
+            path: '/pages',
+            component: view('default'),
+            redirect: '/404',
+            children: [{
+                path: ':id',
+                component: view('page_details'),
+                name: 'pageDetails'
+            }]
+        },
+        {
+            path: '/404',
+            name: '404',
+            component: view('notfoundcomponent')
+        },
+        {
+            path: '*',
+            redirect: '/404'
         }
-      ]
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: view('notfoundcomponent')
-    },
-    {
-      path: '*',
-      redirect: '/404'
-    }
-  ]
+    ]
 
-  /**
-   * Asynchronously load view (lazy-loading)
-   * @param {string} name the filename (basename) of the view to load.
-   */
-  function view(name) {
-    return function(resolve) {
-      require(['vue!' + name + '.vue'], resolve);
-    }
-  };
+    /**
+     * Asynchronously load view (lazy-loading)
+     * @param {string} name the filename (basename) of the view to load.
+     */
+    function view(name) {
+        return function(resolve) {
+            require(['vue!' + name + '.vue'], resolve);
+        }
+    };
 });
