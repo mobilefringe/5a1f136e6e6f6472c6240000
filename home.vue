a9282f7c7ed7ec9d6dd9a2589c74723144588ae8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-13 21:31:33 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -88,7 +88,7 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "vue!today_hours", "vue!vue-slick","moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue,Vuex, TodayHoursComponent, slick ,moment, tz, VueMoment, Meta) {
+    define(["Vue", "vuex", "vue!today_hours", "vue!vue-slick","moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, sVuex, TodayHoursComponent, slick ,moment, tz, VueMoment, Meta) {
         return Vue.component("home-component", {
             template: template, // the variable template will be injected
             data: function() {
@@ -107,7 +107,8 @@
                 }
             },
             created () {
-                this.$store.dispatch("getData", "banners")  
+                this.$store.dispatch("getData", "banners");
+                this.$store.dispatch("getData", "feature_items")
             },
             computed: {
                 ...Vuex.mapGetters([
