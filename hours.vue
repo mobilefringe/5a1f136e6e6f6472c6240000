b2178861ebeea3944c5f0404e1a5bfeb26846f66
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-13 22:01:29 -0500

    Canyon Crest 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -72,7 +72,7 @@
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
-                    'hours'
+                    'hours',
                 ]),
                 timezone () {
                   return this.$store.getters.getTimezone;
