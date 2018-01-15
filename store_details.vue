d097f3fc7f4134a729020f3dfaaea42846d4126b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-15 17:37:58 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -236,7 +236,7 @@
                 },
                 dropPin () {
                     console.log(this.currentStore);
-                    console.log(this.currentStore.svgmap_region);
+                    console.log("this.currentStore.svgmap_region", this.currentStore.svgmap_region);
                     // this.svgMapRef.hideMarkers();
                     this.svgMapRef.addMarker(this.currentStore,'//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
                     this.svgMapRef.setViewBox(this.currentStore)
