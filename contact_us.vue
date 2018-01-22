5169b71df9701167e369da4916b8dc622d70ba66
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-22 10:20:26 -0500

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -160,10 +160,18 @@
             },
             methods: {
                 updateLeasingInfo () {
-                    this.currentPromo = this.findPromoBySlug(id);
-                    if (this.currentPromo === null || this.currentPromo === undefined){
-                        this.$router.replace({ name: '404'});
-                    }
+                    this.$store.dispatch('LOAD_PAGE_DATA', {
+                        url: this.property.mm_host + "/pages/" + id + ".json"
+                    }).then(response => {
+                        // this.dataLoaded = true;
+                        this.currentPage = response.data;
+                        console.log(this.currentPage);
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        this.$router.replace({
+                            name: '404'
+                        });
+                    });
                 },
                 validateBeforeSubmit() {
                     this.$validator.validateAll().then((result) => {
