24e65696fbabc9fd8036a159c0c3e48966575bc1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-08 10:23:12 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -83,7 +83,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-12 promo_item" id="promos_main" v-if="currentStore && currentStore.total_published_promos > 0">
+            <div class="col-md-12 promo_item" id="promos_main" v-if="storeHours.length > 0 ">
                 <h2 class="store_details_promo_heading sub_title">
                     <!--<img src="//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456507166000/promo_icon.png" class="" alt="promo icon">-->
                     {{currentStore.name}} Promotions
