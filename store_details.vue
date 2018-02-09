c627676f51165dd2e57fc4b38f06fdc98b701004
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-09 09:53:19 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -94,7 +94,6 @@
             </div>
             <div class="col-md-12 promo_item" id="jobs_main" v-if="currentStore && currentStore.total_published_jobs > 0">
                 <h2 class="store_details_promo_heading sub_title">
-                    <!--<img src="//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456507192000/jobs_icon.png" class="" alt="jobs icon">-->
                     {{currentStore.name}} Jobs
                 </h2>
                 <div id="jobs_container">
