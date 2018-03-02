a2801e8c090fdfc3d23d2c9240985763c85dca4f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-02 10:03:15 -0500

    Canyon Crest 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -10,7 +10,7 @@
                     <div class="map_container">
                         <div class="map_storelist hidden_phone">
                             <v-select :options="allStores" :placeholder="'Select A Store'" :searchable="false" :label="'name'" :on-change="dropPin"></v-select>                         </div>
-                        <svg-map ref="svgRef" v-bind:svgMapUrl="getSVGurl" :regions="regions"></svg-map>
+                        <!--<svg-map ref="svgRef" v-bind:svgMapUrl="getSVGurl" :regions="regions"></svg-map>-->
                     </div>
                     <div>
                         <mapplic-map ref="mapplic_ref" :height="700" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList"></mapplic-map>
