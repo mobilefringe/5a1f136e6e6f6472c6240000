12f7b370841c86db12375772e409044c0ceeed59
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-21 15:40:04 -0400

    Canyon Crest 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -17,7 +17,7 @@
                         <div class="form-group ">
                             <div class="col-sm-12 col-xs-12" :class="{'has-error': errors.has('name')}">
                                 <label class="label" for="name">Name</label>
-                                <input v-model="form_data.name" v-validate="'required'" class="form-control" :class="{'input': true}" name="name" type="text" placeholder="Name" data-vv-delay="1000">
+                                <input v-model="form_data.name" v-validate="'required'" class="form-control" :class="{'input': true}" name="cm-name" type="text" placeholder="Name" data-vv-delay="1000">
                                 <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
                             </div>
                         </div>
