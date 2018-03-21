1f865a000102e20b8d879c3e6f30eccd3dab751a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-21 12:49:13 -0400

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -164,9 +164,7 @@
                         console.log(this.leasingPage);
                     }, error => {
                         console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                        this.$router.replace({
-                            name: '404'
-                        });
+                        this.$router.replace('/');
                     });
                 },
                 validateBeforeSubmit() {
