ea38858ad68193015aabd009b4883c88fbc633bd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-21 15:29:05 -0400

    Canyon Crest 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -2,14 +2,49 @@
     <div class=" main_container" id="promotions_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="margin_25_across padding_top_40">
             <div class="row">
-                <div class="col-sm-12 text-center">
+            <div class="col-sm-5 contact_contents padding_top_20">
+                    <form class="form-horizontal" action="form-submit" @submit.prevent="validateBeforeSubmit">
+                        <div class="form-group ">
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('name')}">
+                                <label class="label" for="name">Name</label>
+                                <input v-model="form_data.name" v-validate="'required'" class="form-control" :class="{'input': true}" name="name" type="text" placeholder="Name" data-vv-delay="1000">
+                                <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('email')}">
+                                <label class="label" for="email">Email</label>
+                                <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="1000">
+                                <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
+                            </div>
+                        </div>
+                        <div class="form-group account-btn text-left m-t-10">
+                            <div class="col-xs-12">
+                                <button class="animated_btn" type="submit" :disabled="formSuccess">Subscribe</button>
+                            </div>
+                        </div>
+                    </form>
+                    
+                    <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">Success</span>
+                        Thank you for contacting us. A member from our team will contact you shortly.
+                    </div>
+                    <div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <span class="sr-only">Error:</span>
+                        There was an error when trying to submit your request. Please try again later.
+                    </div>
+                    
+                </div>
+                <div class="col-sm-7 text-center">
                     <img style="margin-bottom:20px; max-width:100%;" src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1512580269422/placeholder_contact_image.png" alt="plaza">
                 </div> 
             </div>
             <hr/>
-            <div class="row"> 
+            <!--<div class="row"> -->
                 
-            </div>
+            <!--</div>-->
             <div class="padding_top_40"></div>    
         </div>
     </div>
