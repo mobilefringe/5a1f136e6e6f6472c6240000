16982928e38cf4a005f44bb8d7198f4a31506c45
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-27 10:16:16 -0400

    Canyon Crest 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -136,7 +136,6 @@
                         url: this.property.mm_host + "/pages/canyoncrest-leasing-info.json"
                     }).then(response => {
                         this.leasingPage = response.data;
-                        console.log(this.leasingPage);
                     }, error => {
                         console.error("Could not retrieve data from server. Please check internet connection and try again.");
                         // this.$router.replace('/');
