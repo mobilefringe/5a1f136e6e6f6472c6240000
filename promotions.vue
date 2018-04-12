3c0b8440de8dc26239747cae0986598cf4813616
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-12 13:39:29 -0400

    Canyon Crest 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -61,7 +61,7 @@
                     var temp_promo = [];
                     _.forEach(this.processedPromos, function(value, key) {
                         today = moment().tz(vm.timezone);
-                        webDate = moment(value.show_on_web_date).tz(vm.timezone)
+                        webDate = moment(value.show_on_web_date).tz(vm.timezone);
                         if (today.format('DMY') >= webDate.format('DMY')) {
                             value.description_short = _.truncate(value.description, {
                                 'length': 150
