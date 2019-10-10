07bbafb8a24cc1c169136ef5bd05e95a7f73f107
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-10 12:35:30 -0400

    Canyon Crest 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -70,7 +70,7 @@
                                 'length': 150
                             });
                             if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
-                                value.store.store_front_url_abs = vm.operty.default_logo_url;
+                                value.store.store_front_url_abs = vm.property.default_logo_url;
                             }
                             else if (value.store == null || value.store == undefined) {
                                 value.store = {};
